a87b07bc2a1fe4566e316b347a4dd955a60bdbaa
face0ff <facekrakadil@gmail.com>
2023-06-20 08:45:11 +0200

    ny uje chtoto

--- swipe/conftest.py
+++ swipe/conftest.py
@@ -2,6 +2,8 @@ import pytest
 from allauth.account.models import EmailAddress
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import AccessToken
+
+import user_app.serializers
 from user_app.models import User
 
 
@@ -13,19 +15,30 @@ def api_client():
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        User.objects.create_superuser(email='admin@example.com', password='string')
-        User.objects.create_user(email='user@example.com', password='string', role='user')
-        User.objects.create_user(email='owner@example.com', password='string', role='owner')
+        admin = User.objects.create_superuser(email='admin@example.com', username='admin@example.com',password='string', role='manager')
+        EmailAddress.objects.create(email=admin.email, verified=True, primary=True, user_id=admin.id)
+        user = User.objects.create_user(email='user@example.com', username='user@example.com', password='string', role='user')
+        EmailAddress.objects.create(email=user.email, verified=True, primary=True, user_id=user.id)
+        owner = User.objects.create_user(email='owner@example.com', username='owner@example.com', password='string', role='owner')
+        EmailAddress.objects.create(email=owner.email, verified=True, primary=True, user_id=owner.id)
 
 @pytest.fixture
-def create_user(email, password):
-    user = User.objects.create_user(email=email, password=password, username=email, role='user')
-    EmailAddress.objects.create(email=email, verified=True, primary=True, user_id=user.id)
-    return user
+def create_user():
+    return User.objects.get(role='user')
 
 
 @pytest.fixture()
 def user_authenticate(api_client, create_user):
-    user = create_user('test@example.com', 'password')
-    api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {AccessToken.for_user(user)}')
+    api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {AccessToken.for_user(create_user)}')
+    return api_client
+
+
+@pytest.fixture
+def create_admin():
+    return User.objects.get(role='manager')
+
+
+@pytest.fixture()
+def admin_authenticate(api_client, create_admin):
+    api_client.credentials(HTTP_AUTHORIZATION=f'Bearer {AccessToken.for_user(create_admin)}')
     return api_client
--- swipe/user_app/tests/test_notarius.py
+++ swipe/user_app/tests/test_notarius.py
@@ -2,12 +2,45 @@ import pytest
 
 from user_app.models import Notaries
 
+pytestmark = pytest.mark.django_db
 
-@pytest.mark.django_db
-def test_notarius():
-    # print('11111111111111111')
-    # Notaries.objects.create(first_name='Notarius 1')
-    # Notaries.objects.create(first_name='Notarius 2')
-    # response = user_authenticate.get('notaries')
-    # assert response.status_code == 200
-    assert True==1
+
+class TestNotaries:
+    endpoint = '/api/v1/notaries/'
+
+    @pytest.fixture
+    def create_one(self):
+        one = Notaries.objects.create(first_name='Notaries 1')
+        return one
+
+    def test_get_notarius(self, user_authenticate):
+        response = user_authenticate.get(self.endpoint)
+        assert response.status_code == 200
+
+    def test_post_notaries(self, admin_authenticate):
+        value = {
+            "first_name": "string",
+            "last_name": "string",
+            "telephone": "string",
+            "email": "user@example.com"
+        }
+        response = admin_authenticate.post(self.endpoint, data=value)
+        assert response.status_code == 201
+
+    def test_retrieve_notaries(self, user_authenticate, create_one):
+        response = user_authenticate.get(f'{self.endpoint}{create_one.id}/')
+        assert response.status_code == 200
+
+    def test_put_notaries(self, admin_authenticate, create_one):
+        value = {
+            "first_name": "1231",
+            "last_name": "string",
+            "telephone": "string",
+            "email": "user@example.com"
+        }
+        response = admin_authenticate.put(f'{self.endpoint}{create_one.id}/', data=value)
+        assert response.status_code == 200
+
+    def test_delete_notaries(self, admin_authenticate, create_one):
+        response = admin_authenticate.delete(f'{self.endpoint}{create_one.id}/')
+        assert response.status_code == 204
